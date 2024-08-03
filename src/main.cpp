bc11fafb8ba5a16734e13653b2766c9d993742a7
translibrius <daumantas.balakauskas@gmail.com>
2024-08-03 23:55:15 +0300

    basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
-
+#include "raylib.h"
 
 int main(int argc, char* argv[]) {
+	InitWindow(500, 500, "Game of Life");
 
+	while (!WindowShouldClose()) {
+		if (!IsWindowReady()) continue;
+
+		BeginDrawing();
+		EndDrawing();
+	}
+
+	CloseWindow();
 }
